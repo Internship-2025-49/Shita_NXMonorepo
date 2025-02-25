aad0dc4a8ee9f8e53cc2720b60d09eb18e0093a3
Shita Zeny <shita0639@gmail.com>
2025-02-25 11:34:27 +0700

    test: fix getUsers test code #3

--- org/apps/hono/JesTest/UserController.test.ts
+++ org/apps/hono/JesTest/UserController.test.ts
@@ -1,148 +1,60 @@
 import type { Context } from "hono";
 import { createUsers, deleteUser, getUserById, getUsers, updateUser } from "../src/controller/UserController";
-import { db } from "../src/db/index";
+// import { db } from '../src/db/index';
+import { User } from "../src/db/schema";
+import { asc, eq } from "drizzle-orm";
+import { drizzle } from "drizzle-orm/mysql2";
+import * as mysql from "mysql2/promise";
 
-jest.mock("../src/db/index", () => ({
-  db: {
-    select: jest.fn().mockReturnValue({
-      from: jest.fn(),
-    }),
-    insert: jest.fn(),
-    update: jest.fn(),
-    delete: jest.fn(() => ({
-      where: jest.fn().mockResolvedValue(undefined), 
-    })),
+let db: ReturnType<typeof drizzle>;
+let connection: mysql.Connection;
 
-  },
-}));
+beforeAll(async () => {
+  connection = await mysql.createConnection({
+    uri: process.env.DATABASE_URL ?? '',
+    charset: 'utf8mb4'
+  });
 
-describe('UserController test', () => {
-  test("getUsers test", async () => {
+  db = drizzle(connection);
+});
+
+afterAll(async () => {
+  if (connection) {
+    await connection.end(); 
+  }
+});
+
+describe('getUsers test', () => {
+
+  test('getUsers test', async () => {
     const getTest = {
       json: jest.fn(),
     } as unknown as Context;
-  
-    const mockUsers = [{ id: 1, username: "shitaa25", name: "Shita Zeny", address: "Indonesia", phone: "0987654321" }];
-  
-    (db.select().from as jest.Mock).mockResolvedValue(mockUsers);
-  
+
+    if (!db) {
+      throw new Error("Database connection is not initialized.");
+    }
+
+    const users = await db.select().from(User);
+
     await getUsers(getTest);
-  
-    expect(getTest.json).toHaveBeenCalledWith(mockUsers);
+
+    expect(getTest.json).toHaveBeenCalledWith(users);
   });
 
-  test("getUsersByID test", async () => {
-    const userId = 1;
-
-    const getUserByIdTest = {
+  test('getUserByID test', async () => {
+    const userID = 1;
+    const getUserByIDTest = {
       req: {
-          param: jest.fn().mockReturnValue(userId),
-      },
-      json: jest.fn(),
+        param: jest.fn().mockReturnValue(userID),
+      }, json: jest.fn(), 
     } as unknown as Context;
 
-    const mockUsers = [{id: 1, username: "shitaa25", name: "Shita Zeny", address: "Indonesia", phone: "0987654321"}];
+    const users = await db.select().from(User).where(eq(User.id, userID));
 
-    (db.select().from as jest.Mock).mockReturnValue({
-      where: jest.fn().mockReturnValue(mockUsers),
-    });
+    await getUserById(getUserByIDTest);
 
-    await getUserById(getUserByIdTest);
+    expect(getUserByIDTest.json).toHaveBeenCalledWith(users);
+  })
 
-    expect(getUserByIdTest.json).toHaveBeenCalledWith(mockUsers)
-  });
-
-  test("createUsers test", async () => {
-    const createTest = {
-      req: {
-        json: jest.fn().mockResolvedValue({
-          username: "user test",
-          name: "nama test",
-          address: "alamat address",
-          phone: "12345678890",
-        }),
-      },
-      json: jest.fn(), 
-    } as unknown as Context;
-  
-    const mockUser = {
-      username: "user test",
-      name: "nama test",
-      address: "alamat address",
-      phone: "12345678890",
-    };
-  
-    (db.insert as jest.Mock).mockReturnValue({
-      values: jest.fn().mockResolvedValue(mockUser),
-    });
-  
-    await createUsers(createTest); 
-  
-    expect(createTest.json).toHaveBeenCalledWith(mockUser);
-  });
-  
-  test("updateUsers test", async () => {
-    const userId = 1;
-  
-    const updateTest = {
-      req: {
-        param: jest.fn().mockReturnValue(userId),
-        json: jest.fn().mockResolvedValue({
-          username: "updatedUser",
-          name: "Updated Name",
-          address: "Updated Address",
-          phone: "9876543210",
-        }),
-      },
-      json: jest.fn(),
-    } as unknown as Context;
-  
-    const mockUpdatedUser = [
-      {
-        id: userId,
-        username: "updatedUser",
-        name: "Updated Name",
-        address: "Updated Address",
-        phone: "9876543210",
-      },
-    ];
-  
-    (db.update as jest.Mock).mockReturnValue({
-      set: jest.fn().mockReturnValue({
-        where: jest.fn().mockResolvedValue(undefined),
-      }),
-    });
-  
-    (db.select as jest.Mock).mockReturnValue({
-      from: jest.fn().mockReturnValue({
-        where: jest.fn().mockResolvedValue(mockUpdatedUser),
-      }),
-    });
-  
-    await updateUser(updateTest);
-  
-    expect(updateTest.json).toHaveBeenCalledWith(mockUpdatedUser);
-  });
-
-  test("deleteUsers test", async () => {
-    const userId = 1;
-  
-    const deleteTest = {
-      req: {
-        param: jest.fn().mockReturnValue(userId),
-      },
-      json: jest.fn(),
-    } as unknown as Context;
-  
-    (db.delete as jest.Mock).mockReturnValue({
-      where: jest.fn().mockResolvedValue(undefined),
-    });
-  
-    await deleteUser(deleteTest);
-  
-    expect(deleteTest.json).toHaveBeenCalledWith({
-      statusCode: 200,
-      message: "User Data was Deleted Successfully!",
-    });
-  });
 })
