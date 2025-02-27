37d6dde108f072be893b3c0af07acf324e88de0d
Shita Zeny <shita0639@gmail.com>
2025-02-27 09:45:10 +0700

    feat: temporary frontend2 #4

--- org/apps/shared/components/src/shared-ui.ts
+++
@@ -1,7 +0,0 @@
-// export * from './ui/button';
-// export * from './ui/table';
-// export * from './ui/input';
-// export * from './ui/card';
-// export * from './ui/form';
-// export * from './ui/textarea';
-// export * from './ui/label';
